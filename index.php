23c8ebb99e539478eb6e81dcd50ea3bfbed0405f
neostarsky97 <cooperalan97@gmail.com>
2018-04-09 14:01:18 +0530

    Second commit, adding album UI and database content

--- index.php
+++ index.php
@@ -1,106 +1,24 @@
-<?php
+<?php include('includes\header.php'); ?>
 
-  include_once('C:\xampp\htdocs\Overture\includes\config.php');
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
-  // session_destroy(); LOGOUT
+<div class="gridViewContainer">
 
-  if(isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-  } else {
-    // $urlEncode = urlencode("Location: \Overture\register.php");
-    header("Location: register.php");
-  }
-?>
+  <?php
+    $stmt = $con->query("SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+    foreach ($stmt as $row) {
+      $artPath = $row['artworkPath'];
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Home Page</title>
-    <link rel="stylesheet" type="text/css"href="assets/css/style.css">
-  </head>
-  <body>
-    <?php echo $userLoggedIn; ?>
-    <div id="nowPlayingContainer">
-      <div id="nowPlayingBar">
-          <div id="nowPlayingLeft">
-            <div class="content">
+      echo "<div class='gridViewItem'>
+              <a href='album.php?id=". $row['id'] ."'>".
+                "<img src='$artPath'></img> " .
+                  "<div class='gridViewInfo'>"
+                    . $row['title'] .
+                  "</div>
+                </a>
+            </div>";
+    }
+  ?>
 
-              <div class="albumLink">
-                <img src="assets/images/download (1).png" alt="Artwork" class="albumArtwork">
-              </div>
-
-              <div class="trackInfo">
-                <div class="trackName">
-                  <span>Hey Brother</span>
-                </div>
-                <div class="artistName">
-                  <span>Avicii</span>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-          <div id="nowPlayingCenter">
-              <div class="content playerControls">
-                <div class="buttons">
-
-                  <button class="controlButton Shuffle" title="Shuffle button">
-                    <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                  </button>
-
-                  <button class="controlButton Previous" title="Previous button">
-                    <img src="assets/images/icons/previous.png" alt="Previous">
-                  </button>
-
-                  <button class="controlButton Play" title="Play button">
-                    <img src="assets/images/icons/play.png" alt="Play">
-                  </button>
-
-                  <button class="controlButton Pause" title="Pause button">
-                    <img src="assets/images/icons/pause.png" alt="Pause">
-                  </button>
-
-                  <button class="controlButton Next" title="Next button">
-                    <img src="assets/images/icons/next.png" alt="Next">
-                  </button>
-
-                  <button class="controlButton Repeat" title="Repeat button">
-                    <img src="assets/images/icons/repeat.png" alt="Repeat">
-                  </button>
-                </div>
-
-                <div class="playBackBar">
-
-                  <div class="progressTime Current">
-                    <span>0.00</span>
-                  </div>
-
-                  <div class="progressBar">
-                    <div class="progressBarBg">
-                      <div class="progress">
-
-                      </div>
-                    <div class="progressBarSeek">
-
-                    </div>
-                    </div>
-                  </div>
-
-                  <div class="progressTime Remaining">
-                    <span>0.00</span>
-                  </div>
-
-                </div>
-
-              </div>
-          </div>
-
-          <div id="nowPlayingRight">
-
-          </div>
-      </div>
-    </div>
-  </body>
-</html>
+</div>
+<?php include('includes\footer.php'); ?>
